193d15840bd08189330f83784f2d8b6894190b27
HenriqueSimas <henrique.abecassimas@gmail.com>
2024-03-20 09:51:37 -0400

    pasta de exercicios de nodejs

---
+++ Nodejs/node1/index.js
@@ -0,0 +1,46 @@
+const http = require('http');
+fs = require('fs');
+path = require('path');
+url = require('url');
+utils = require('./utils');
+dirPath = process.argv[2], dotenv = require('dotenv');
+dotenv.config({path: `.env.${process.env.NODE_ENV}`});
+console.log(process.env.NODE_ENV);
+const PORT = process.env.PORT;
+
+const server = http.createServer((req, res) => {
+    let reqUrl = url.parse(req.url, true);
+    filePath = path.join(dirPath, reqUrl.pathname);
+
+    fs.stat(filePath, (err, stats) => {
+        
+        if (err) { res.writeHead(404, {'Content-Type': 'text/html;charset=utf-8'}); 
+        res.end('Arquivo não encontrado'); }
+        
+        else if (stats.isDirectory()) {
+            fs.readdir(filePath, (err, files) => {
+                if (err) { res.writeHead(500, {'Content-Type': 'text/html;charset=utf-8'});
+                 return res.end('Erro ao ler o diretório'); }
+                
+                else {
+                    let fileLinks = '';
+                    files.forEach(file => { fileLinks += utils.createLink(file); });
+                    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'}); 
+                    res.end(fileLinks);
+                }
+            });
+        } 
+        else {
+            fs.readFile(filePath, 'utf8', (err, data) => {
+                if (err) { 
+                res.writeHead(500, {'Content-Type': 'text/html;charset=utf-8'}); 
+                return res.end('Erro ao ler o arquivo'); }
+                
+                res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'}); 
+                res.end(`${data}<br><a href="/">Voltar</a>`);
+            });
+        }
+    });
+});
+
+server.listen(PORT, () => { console.log(`Servidor rodando na porta ${PORT}`); });
